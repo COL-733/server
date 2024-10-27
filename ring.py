cba9025d7f9f8267099685ed40885008f24248bf
Kotikala Raghav <koraghav@outlook.com>
2024-10-27 14:11:43 +0530

    init

---
+++ ring.py
@@ -0,0 +1,24 @@
+from sortedcontainers import SortedList
+from server import ServerInfo
+
+
+class VirtualNode:
+    pass
+
+class VirtualNode:
+    def __init__(self, server: ServerInfo, pos: int):
+        self.server: ServerInfo = server
+        self.pos: int = pos
+
+    def move(self, pos: int):
+        self.pos = pos
+        # TODO: Move Keys
+
+    def __lt__(self, other: VirtualNode) -> bool:
+        return self.pos < other.pos
+
+class Ring:
+    def __init__(self):
+        self.state: SortedList[VirtualNode] = SortedList()
+        self.servers: set[ServerInfo] = set()
+        
---
+++ server.py
@@ -0,0 +1,6 @@
+class ServerInfo:
+    def __init__(self, name: str, port: int):
+        self.name: str = name
+        self.port: int = port
+        
+
