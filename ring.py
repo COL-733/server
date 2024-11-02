d2ce336cfc6c1b61ab21f1fd92d3663b3443e320
Kotikala Raghav <koraghav@outlook.com>
2024-11-02 12:05:07 +0530

    Add random tokens to ring

--- ring.py
+++ ring.py
@@ -1,24 +1,25 @@
+import random
 from sortedcontainers import SortedList # type: ignore
-from server import ServerInfo
+from server import ServerInfo, Server
 
+MAX = (1 << 128) - 1
 
 class VirtualNode:
     pass
 
 class VirtualNode:
-    def __init__(self, server: ServerInfo, pos: int):
-        self.server: ServerInfo = server
+    def __init__(self, server: str, pos: int):
+        self.server: str = server
         self.pos: int = pos
-
-    def move(self, pos: int):
-        self.pos = pos
-        # TODO: Move Keys
 
     def __lt__(self, other: VirtualNode) -> bool:
         return self.pos < other.pos
 
 class Ring:
-    def __init__(self):
+    def __init__(self, numTokens, name):
         self.state: SortedList[VirtualNode] = SortedList()
-        self.servers: set[ServerInfo] = set()
-        
+        self.name = name
+
+        for _ in range(numTokens):
+            self.state.add(VirtualNode(name, random.randint(0, MAX)))
+    
