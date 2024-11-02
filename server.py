53cc5dcc6c8cf1bce822042305b897cd29061596
Magnificientmouse <jppatel.pp95@gmail.com>
2024-11-02 18:00:10 +0530

    Server Connect to Switch

--- server.py
+++ server.py
@@ -5,23 +5,35 @@ from storage import Storage
 import socket
 # from collections import deque
 import queue
-    
-class ServerInfo:
-    def __init__(self, name: str, ip: str, switch_ip: str , port: int):
-        self.ip: str = ip
-        self.switch: str = switch_ip # or directly socket
-        self.name: str = name
-        self.port: int = port
+import time
+
+SWITCH_PORT = 2000
         
 class Server(Process):
-    def __init__(self, switch_name: str, switch_ip: str):
-        super(Server, self).__init__()
-        self.name: str
-        self.switch: str = switch_ip
-        self.socket: socket.socket 
+    def __init__(self, switch_name: str, switch_ip: str, port: int):
+        self.name: str = f'{switch_name}_{port}'
+        super(Server, self).__init__(info=self.name)
+
+        self.name: str = f'{switch_name}_{port}'
+        self.port: int = port
+        self.switch_name: str = switch_name
+        self.switch_ip: str = switch_ip
+
+        # Connect to Switch
+        self.socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connectSwitch()
+
         self.ring: Ring
-        self.storage: Storage = Storage('localhost','root','Jdsp9595@',f'{self.info.name}_storage')
-        self.cmdQueue: queue[MessageType] 
+        self.storage: Storage = Storage('localhost','root','Jdsp9595@',f'{self.name}_storage')
+        self.cmdQueue: queue[MessageType]
+
+    def connectSwitch(self) -> None: # Connect to switch
+        while True:
+            try:
+                self.socket.connect('switch_ip',SWITCH_PORT)
+                break
+            except ConnectionRefusedError:
+                time.sleep(0.2)
 
     def recv():
         pass
@@ -47,9 +59,12 @@ class Server(Process):
     def sendHintedHandoffs(): # Thread
         pass
 
-    def run():
-        # Start the server process
-        cmd_q = queue.
+    def run(self):
+        # command queue
+        cmd_q = queue.Queue()
+
+
+
         # Start the recv thread
         # Start the cmd handler thread.
         pass
